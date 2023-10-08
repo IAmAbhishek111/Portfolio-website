cd2519c15b77b4de1eb748fc703c20b81c0ff403
ABHISHEK KUMAR JHA <abhishek.jha8387@gmail.com>
2023-10-08 23:26:15 +0530

    target blank fixed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -69,8 +69,8 @@ const Portfolio = () => {
                 </div>
                 <h3>{title}</h3>
                 <div className='portfolio__item-cta'>
-                  <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
+                  <a href={github} className='btn' target='_blank' rel='noopener'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel='noopener'>Live Demo</a>
                 </div>
               </article>
             )
