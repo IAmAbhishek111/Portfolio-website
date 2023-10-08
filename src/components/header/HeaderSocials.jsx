ef580dc34897754e717a2b6fc76b747083aad8f2
ABHISHEK KUMAR JHA <abhishek.jha8387@gmail.com>
2023-10-08 23:31:48 +0530

    target blank fixed

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,9 +7,9 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/abhishek141100/" target='_blank' rel='noopener'><BsLinkedin/></a>
-        <a href="https://github.com/IAmAbhishek111" target='_blank' rel='noopener'><BsGithub/></a>
-        <a href="https://www.instagram.com/_.abhishek._777/" target='_blank' rel='noopener'><BsInstagram/></a>
+        <a href="https://www.linkedin.com/in/abhishek141100/" target='_blank' rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/IAmAbhishek111" target='_blank' rel="noreferrer"><BsGithub/></a>
+        <a href="https://www.instagram.com/_.abhishek._777/" target='_blank' rel="noreferrer"><BsInstagram/></a>
        
     </div>
   )
